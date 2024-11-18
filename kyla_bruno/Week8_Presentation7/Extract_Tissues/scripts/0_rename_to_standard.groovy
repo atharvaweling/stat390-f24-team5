9da2caeb4260b7a276351780d57811f4836083a5
kbruno8203 <101294729+kbruno8203@users.noreply.github.com>
2024-11-18 09:05:09 -0600

    Update 0_rename_to_standard.groovy

--- kyla_bruno/Week8_Presentation7/Extract_Tissues/scripts/0_rename_to_standard.groovy
+++ kyla_bruno/Week8_Presentation7/Extract_Tissues/scripts/0_rename_to_standard.groovy
@@ -1,5 +1,7 @@
-def inputDir = new File("Users/kylabruno/Downloads/Stat_390/needs_manual_intervention") // Replace with path to test folder
-//def inputDir = new File("Volumes/DATA_STORE/raw_data_liverpool")
+// NOT A FINALIZED VERSION
+// this was just used in my own testing of the program
+// will update if needed, but Noah also has a script that does this and Cara too
+def inputDir = new File("Users/kylabruno/Downloads/Stat_390/needs_manual_intervention") // Replace with path to images
 
 // Standardize filenames in the input directory
 inputDir.eachFile { file ->
